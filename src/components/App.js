bbd171f080876702996c8fcadd14e5263621e574
Ben Hurst <BHurst8@gmail.com>
2018-11-24 12:17:00 -0800

    Background/animation styling

--- src/components/App.js
+++ src/components/App.js
@@ -20,6 +20,7 @@ export default class App extends React.Component {
   getFlights(origin, destination) {
     let baseURL = window.location.hostname === 'localhost'? 'http://localhost:5000': 'https://flightfinderapi.azurewebsites.net';
     let url = `${baseURL}/api/flights/search?from=${origin}&to=${destination}`;
+
     superagent.get(url)
       .then(results => {
         this.setState({hasError: false});
@@ -57,9 +58,13 @@ export default class App extends React.Component {
           <h1>Imaginary Airlines</h1>
         </header>
         <main> 
-          <h1 className="pageTitle">Flight Finder</h1>
-          <SearchForm searchClass={this.state.hasError ? 'error' : 'success'} flightSearch=   {this.getFlights} airports={this.state.airports}/>
-          <SearchResults searchResults={this.state.flights} sortFlights={this.sortFlights}/>
+          <section id="page1">
+            <h1 className="pageTitle">flight<span className="finder">finder</span></h1>
+            <SearchForm searchClass={this.state.hasError ? 'error' : 'success'} flightSearch=   {this.getFlights} airports={this.state.airports}/>
+          </section>
+          <section id="page2" className={this.state.flights.length? this.state.flights[0].to:null}>
+            <SearchResults searchResults={this.state.flights} sortFlights={this.sortFlights}/>
+          </section>
         </main>
       </Fragment>
     );
